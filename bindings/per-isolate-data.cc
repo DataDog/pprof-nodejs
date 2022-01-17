55e0581375186f0753d0ba18d7bdf7c4231d679f
Stephen Belanger <stephen.belanger@datadoghq.com>
2022-01-17 11:37:08 -0800

    Add CPU Profiler with Code Hotspots support

---
+++ bindings/per-isolate-data.cc
@@ -0,0 +1,37 @@
+#include <unordered_map>
+
+#include "per-isolate-data.hh"
+
+namespace dd {
+
+static std::unordered_map<v8::Isolate*, PerIsolateData*> per_isolate_data_;
+
+PerIsolateData::PerIsolateData(v8::Isolate* isolate) {
+  per_isolate_data_[isolate] = this;
+  node::AddEnvironmentCleanupHook(isolate, [](void* data) {
+    delete static_cast<PerIsolateData*>(data);
+  }, this);
+}
+
+PerIsolateData* PerIsolateData::For(v8::Isolate* isolate) {
+  auto maybe = per_isolate_data_.find(isolate);
+  if (maybe != per_isolate_data_.end()) {
+    return maybe->second;
+  }
+
+  return new PerIsolateData(isolate);
+}
+
+Nan::Global<v8::Function>& PerIsolateData::CpuProfilerConstructor() {
+  return cpu_profiler_constructor;
+}
+
+Nan::Global<v8::Function>& PerIsolateData::LocationConstructor() {
+  return location_constructor;
+}
+
+Nan::Global<v8::Function>& PerIsolateData::SampleConstructor() {
+  return sample_constructor;
+}
+
+} // namespace dd
---
+++ bindings/per-isolate-data.hh
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <nan.h>
+#include <node.h>
+#include <v8.h>
+
+namespace dd {
+
+class PerIsolateData {
+ private:
+  Nan::Global<v8::Function> cpu_profiler_constructor;
+  Nan::Global<v8::Function> location_constructor;
+  Nan::Global<v8::Function> sample_constructor;
+
+ public:
+  PerIsolateData(v8::Isolate* isolate);
+
+  static PerIsolateData* For(v8::Isolate* isolate);
+
+  Nan::Global<v8::Function>& CpuProfilerConstructor();
+  Nan::Global<v8::Function>& LocationConstructor();
+  Nan::Global<v8::Function>& SampleConstructor();
+};
+
+} // namespace dd
