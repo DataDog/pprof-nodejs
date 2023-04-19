e4f505df055d15ad8f43820de1409feabdc49263
Attila Szegedi <attila.szegedi@datadoghq.com>
2023-04-19 18:28:34 +0200

    Initial code for code hotspots in wall profiler

---
+++ bindings/buffer.hh
@@ -0,0 +1,76 @@
+#pragma once
+
+namespace dd {
+template <class T>
+class RingBuffer {
+ public:
+  explicit RingBuffer(size_t size)
+      : buffer(std::make_unique<T[]>(size)),
+        capacity_(size),
+        size_(0),
+        back_index_(0),
+        front_index_(0) {}
+
+  bool full() const { return size_ == capacity_; }
+  bool empty() const { return size_ == 0; }
+
+  T& front() { return buffer[front_index_]; }
+
+  const T& front() const { return buffer[front_index_]; }
+
+  void push_back(T t) {
+    if (full()) {
+      if (empty()) {
+        return;
+      }
+      // overwrite buffer head
+      buffer[back_index_] = std::move(t);
+      increment(back_index_);
+      // move buffer head
+      front_index_ = back_index_;
+    } else {
+      buffer[back_index_] = std::move(t);
+      increment(back_index_);
+      ++size_;
+    }
+  }
+
+  void push_front(T t) {
+    if (full()) {
+      if (empty()) {
+        return;
+      }
+      // overwrite buffer head
+      decrement(front_index_);
+      buffer[front_index_] = std::move(t);
+      // move buffer head
+      back_index_ = front_index_;
+    } else {
+      decrement(front_index_);
+      buffer[front_index_] = std::move(t);
+      ++size_;
+    }
+  }
+
+  T pop_front() {
+    auto idx = front_index_;
+    increment(front_index_);
+    --size_;
+    return std::move(buffer[idx]);
+  }
+
+ private:
+  void increment(size_t& idx) const {
+    idx = idx + 1 == capacity_ ? 0 : idx + 1;
+  }
+  void decrement(size_t& idx) const {
+    idx = idx == 0 ? capacity_ - 1 : idx - 1;
+  }
+
+  std::unique_ptr<T[]> buffer;
+  size_t capacity_;
+  size_t size_;
+  size_t back_index_;
+  size_t front_index_;
+};
+}  // namespace dd
--- bindings/profilers/wall.hh
+++ bindings/profilers/wall.hh
@@ -2,6 +2,7 @@
 
 #include <nan.h>
 #include <v8-profiler.h>
+#include "../buffer.hh"
 #include "../wrap.hh"
 
 namespace dd {
@@ -14,10 +15,29 @@ class WallProfiler : public Nan::ObjectWrap {
 
   struct SampleContext {
     std::shared_ptr<LabelWrap> labels;
-    int64_t timestamp; 
+    uint64_t timestamp;
+
+    // Needed to initialize ring buffer elements
+    SampleContext() {}
+
+    SampleContext(std::shared_ptr<LabelWrap> l, uint64_t t)
+        : labels(l), timestamp(t) {}
+
+    SampleContext(SampleContext&& rval) {
+      labels = std::move(rval.labels);
+      timestamp = rval.timestamp;
+    }
+
+    SampleContext& operator=(SampleContext&& rval) {
+      labels = std::move(rval.labels);
+      timestamp = rval.timestamp;
+      return *this;
+    }
   };
 
-  std::vector<SampleContext> contexts_;
+  RingBuffer<SampleContext> contexts;
+  uint64_t current_start;  // start of current profiling run
+  uint64_t last_start;     // start of last profiling run
 
   ~WallProfiler();
   void Dispose();
@@ -27,17 +47,28 @@ class WallProfiler : public Nan::ObjectWrap {
   v8::CpuProfiler* GetProfiler();
 
  public:
-  explicit WallProfiler(int interval);
+  /**
+   * @param intervalMicros sampling interval, in microseconds
+   * @param durationMicros the duration of sampling, in microseconds. This
+   * parameter is informative; it is up to the caller to call the Stop method
+   * every period. The parameter is used to preallocate data structures that
+   * should not be reallocated in async signal safe code.
+   */
+  explicit WallProfiler(int intervalMicros, int durationMicros);
 
   v8::Local<v8::Value> GetLabels();
   void SetLabels(v8::Local<v8::Value>);
+  void UnsetLabels();
 
   void PushContext();
+  void StartImpl(v8::Local<v8::String> name, bool includeLines);
+  void AddLabelSetsByNode(v8::CpuProfile* profile);
 
   static NAN_METHOD(New);
   static NAN_METHOD(Dispose);
   static NAN_METHOD(Start);
   static NAN_METHOD(Stop);
+  static NAN_METHOD(UnsetLabels);
   static NAN_MODULE_INIT(Init);
   static NAN_GETTER(GetLabels);
   static NAN_SETTER(SetLabels);
