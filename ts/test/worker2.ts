305ec631d1023351bc18b074dc683fe8e99beb5b
Attila Szegedi <szegedi@users.noreply.github.com>
2025-09-03 17:41:25 +0200

    Test new features in more places (#225)
    * Exercise async ID collection in more tests
    
    * Make sure async ID collection is off on Windows
    
    * Also turn on useCPED when possible on worker tests
    
    * Observe the number of free handles

--- ts/test/worker2.ts
+++ ts/test/worker2.ts
@@ -1,5 +1,6 @@
 import {parentPort} from 'node:worker_threads';
 import {time} from '../src/index';
+import {satisfies} from 'semver';
 
 const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
 
@@ -8,12 +9,20 @@ const INTERVAL_MICROS = 10000;
 const withContexts =
   process.platform === 'darwin' || process.platform === 'linux';
 
+const useCPED =
+  satisfies(process.versions.node, '>=24.0.0') &&
+  !process.execArgv.includes('--no-async-context-frame');
+
+const collectAsyncId =
+  withContexts && satisfies(process.versions.node, '>=24.0.0');
+
 time.start({
   durationMillis: DURATION_MILLIS,
   intervalMicros: INTERVAL_MICROS,
   withContexts: withContexts,
   collectCpuTime: withContexts,
-  collectAsyncId: false,
+  collectAsyncId: collectAsyncId,
+  useCPED: useCPED,
 });
 
 parentPort?.on('message', () => {
