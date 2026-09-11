4209e12bdf26a3c3dbe96f8997f18e816640cb90
Ilyas Shabi <ilyas.shabi@datadoghq.com>
2026-09-11 14:45:29 +0200

    perf: reuse V8 template for wall profile context objects (#408)

--- bindings/per-isolate-data.hh
+++ bindings/per-isolate-data.hh
@@ -18,12 +18,33 @@
 
 #include <nan.h>
 #include <node.h>
+#include <v8-version.h>
 #include <v8.h>
+#include <array>
+#include <cstddef>
 #include <memory>
+#include <string_view>
+
+// v8::DictionaryTemplate landed in V8 12.3, i.e. Node.js >= 22.
+#if V8_MAJOR_VERSION > 12 || (V8_MAJOR_VERSION == 12 && V8_MINOR_VERSION >= 3)
+#define DD_V8_HAS_DICTIONARY_TEMPLATE 1
+#else
+#define DD_V8_HAS_DICTIONARY_TEMPLATE 0
+#endif
 
 namespace dd {
 
 struct HeapProfilerState;
+
+#if DD_V8_HAS_DICTIONARY_TEMPLATE
+enum class DictionaryTemplateId : size_t {
+  kWallSampleContext,
+  kCount,
+};
+
+constexpr size_t kDictionaryTemplateCount =
+    static_cast<size_t>(DictionaryTemplateId::kCount);
+#endif
 
 class PerIsolateData {
  private:
@@ -31,6 +52,11 @@ class PerIsolateData {
   Nan::Global<v8::Function> allocation_node_constructor;
   Nan::Global<v8::Function> time_profile_node_constructor;
   std::shared_ptr<HeapProfilerState> heap_profiler_state;
+#if DD_V8_HAS_DICTIONARY_TEMPLATE
+  std::array<Nan::Global<v8::DictionaryTemplate>, kDictionaryTemplateCount>
+      dictionary_templates;
+  std::array<size_t, kDictionaryTemplateCount> dictionary_template_arities{};
+#endif
 
   PerIsolateData() {}
 
@@ -41,6 +67,12 @@ class PerIsolateData {
   Nan::Global<v8::Function>& AllocationNodeConstructor();
   Nan::Global<v8::Function>& TimeProfileNodeConstructor();
   std::shared_ptr<HeapProfilerState>& GetHeapProfilerState();
+#if DD_V8_HAS_DICTIONARY_TEMPLATE
+  v8::Local<v8::DictionaryTemplate> GetDictionaryTemplate(
+      v8::Isolate* isolate,
+      DictionaryTemplateId id,
+      v8::MemorySpan<const std::string_view> names);
+#endif
 };
 
 }  // namespace dd
